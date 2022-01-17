451d14a41e9e1171db97269ada8383a8ce65750b
Hans Harck Tønning <hharck2@gmail.com>
2022-01-17 21:58:40 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# VoteExchangeFormat
+
+A description of this package.
---
+++ Tests/VoteExchangeFormatTests/VoteExchangeFormatTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import VoteExchangeFormat
+
+final class VoteExchangeFormatTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(VoteExchangeFormat().text, "Hello, World!")
+    }
+}
